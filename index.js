1e783d9b7874fcaeed15c34779c501de4aeaec4a
Pierce <piercedavidraphael@gmail.com>
2021-10-03 10:42:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
 }
+    
+
